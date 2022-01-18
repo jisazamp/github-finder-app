d10fb6e20c5336cf9c85666b82b53047531de575
Juan P. Isaza <jisazamp@gmail.com>
2022-01-17 23:17:39 -0500

    Added the Footer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 const App = () => {
   return (
-    <div className='flex flex-col justify-between h-screen'>
+    <div className='flex flex-col justify-between h-screen bg-gray-600'>
       <Navbar />
-      <main>Hola mundo</main>
+      <main className='container mx-auto'>Hola mundo</main>
+      <Footer />
     </div>
   );
 };
